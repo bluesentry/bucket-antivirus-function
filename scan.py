8db2b105cf1360962badaec7419f51d160086f31
Dennis Webb <dennis@bluesentryit.com>
2017-11-22 13:42:51 -0600

    Removing break

--- scan.py
+++ scan.py
@@ -64,7 +64,6 @@ def set_av_tags(s3_object, result):
     for tag in curr_tags:
         if tag["Key"] in [AV_STATUS_METADATA, AV_TIMESTAMP_METADATA]:
             new_tags.remove(tag)
-            break
     new_tags.append({"Key": AV_STATUS_METADATA, "Value": result})
     new_tags.append({"Key": AV_TIMESTAMP_METADATA, "Value": datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S UTC")})
     s3_client.put_object_tagging(
