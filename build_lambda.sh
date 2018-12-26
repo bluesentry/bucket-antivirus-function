7c3aec1c2fc70087b1c6ffff3497b96b69427555
Viktor Zavala <viktor@acklenavenue.com>
2018-12-26 04:38:54 -0600

    adding missing packages installation from build_lambda script (#55)

--- build_lambda.sh
+++ build_lambda.sh
@@ -27,10 +27,12 @@ virtualenv env
 pip install --no-cache-dir -r requirements.txt
 
 pushd /tmp
-yumdownloader -x \*i686 --archlist=x86_64 clamav clamav-lib clamav-update
+yumdownloader -x \*i686 --archlist=x86_64 clamav clamav-lib clamav-update json-c pcre2
 rpm2cpio clamav-0*.rpm | cpio -idmv
 rpm2cpio clamav-lib*.rpm | cpio -idmv
 rpm2cpio clamav-update*.rpm | cpio -idmv
+rpm2cpio json-c*.rpm | cpio -idmv
+rpm2cpio pcre*.rpm | cpio -idmv
 popd
 mkdir -p bin
 cp /tmp/usr/bin/clamscan /tmp/usr/bin/freshclam /tmp/usr/lib64/* bin/.
