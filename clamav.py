b0bdb8ce4b64003fc136581f4231f69ed2e77556
Taro Matsuzawa <taro@georepublic.co.jp>
2019-12-18 16:26:49 +0900

    WIP: for python3

--- clamav.py
+++ clamav.py
@@ -42,7 +42,7 @@ RE_SEARCH_DIR = r"SEARCH_DIR\(\"=([A-z0-9\/\-_]*)\"\)"
 
 
 def current_library_search_path():
-    ld_verbose = subprocess.check_output(["ld", "--verbose"])
+    ld_verbose = subprocess.check_output(["ld", "--verbose"]).decode('utf-8')
     rd_ld = re.compile(RE_SEARCH_DIR)
     return rd_ld.findall(ld_verbose)
 
