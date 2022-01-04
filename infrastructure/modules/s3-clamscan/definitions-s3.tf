4ae47aed30141f18da780188f27dc51165955dd7
Dane Charlton <dcharlton@navigatingcancer.com>
2022-01-04 16:07:56 -0500

    fix s3 setup for cleardata controls

--- infrastructure/modules/s3-clamscan/definitions-s3.tf
+++ infrastructure/modules/s3-clamscan/definitions-s3.tf
@@ -22,6 +22,19 @@ data "aws_iam_policy_document" "virus_definitions_bucket_policy" {
             values = ["false"]
         }
     }
+    statement {
+      sid = "AllowSelfLogging"
+      effect = "Allow"
+      principals {
+        type        = "Service"
+        identifiers = ["logging.s3.amazonaws.com"]
+      }
+      actions = [
+        "s3:PutObject",
+        "s3:PutObjectAcl"
+      ]
+      resources = ["arn:aws:s3:::nc-${var.env_name}-s3-clamscan-definitions/self-logs/*"]
+    }
 }
 
 #
@@ -53,4 +66,20 @@ resource "aws_s3_bucket" "virus_definitions" {
   lifecycle_rule {
       enabled = false # this bucket is used to store definitions for the scanner lambda, objects should never be aged out
   }
+  logging {
+    target_bucket = "nc-${var.env_name}-s3-clamscan-definitions"
+    target_prefix = "self-logs/"
+  }
+  versioning {
+    enabled = true
+  }
+}
+
+# Block all public access to our S3 bucket
+resource "aws_s3_bucket_public_access_block" "s3_clamscan_definitions" {
+  bucket = aws_s3_bucket.virus_definitions.id
+  block_public_acls = true
+  block_public_policy = true
+  restrict_public_buckets = true
+  ignore_public_acls = true
 }
