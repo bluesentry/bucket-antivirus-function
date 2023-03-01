e6102f967d4655e7a23da2897cfddf2391a311dc
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2023-03-01 12:09:42 -0300

    compressing not downloading and md5 matches into single line

--- clamav.py
+++ clamav.py
@@ -55,6 +55,8 @@ def current_library_search_path():
 def update_defs_from_s3(s3_client, bucket, prefix):
     create_dir(AV_DEFINITION_PATH)
     to_download = {}
+    older_files = set()
+    md5_matches = set()
     for file_prefix in AV_DEFINITION_FILE_PREFIXES:
         s3_best_time = None
         for file_suffix in AV_DEFINITION_FILE_SUFFIXES:
@@ -65,19 +67,23 @@ def update_defs_from_s3(s3_client, bucket, prefix):
             s3_time = time_from_s3(s3_client, bucket, s3_path)
 
             if s3_best_time is not None and s3_time < s3_best_time:
-                print("Not downloading older file in series: %s" % filename)
+                older_files.add(filename)
                 continue
             else:
                 s3_best_time = s3_time
 
             if os.path.exists(local_path) and md5_from_file(local_path) == s3_md5:
-                print("Not downloading %s because local md5 matches s3." % filename)
+                md5_matches.add(filename)
                 continue
             if s3_md5:
                 to_download[file_prefix] = {
                     "s3_path": s3_path,
                     "local_path": local_path,
                 }
+    print("Not downloading the following older files in series:")
+    print(json.dumps(list(older_files)))
+    print("Not downloading the following files because local md5 matches s3:")
+    print(json.dumps(list(md5_matches)))
     return to_download
 
 
